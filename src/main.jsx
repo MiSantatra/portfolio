0f8ec07c0b5003480ec8148e787048362f1678d1
MiSantatra <miharimbola.ramanantsoa@gmail.com>
2025-07-14 15:56:35 +0300

    update darkmode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const Root = () => {
+  useEffect(() => {
+    document.documentElement.classList.add('dark'); // Adds dark class to <html>
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
